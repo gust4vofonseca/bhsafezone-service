6d1205c2f80425aae51a1227d47c400bfcff3d52
Gustavo Fonseca <gustavofonseca27@gmail.com>
2025-01-25 16:25:01 -0300

    feat: classifier

--- src/modules/classifier/services/classifier.controller.ts
+++ src/modules/classifier/services/classifier.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post } from '@nestjs/common';
 import { ClassifierService } from './classifier.service';
+import { Cron } from '@nestjs/schedule';
+import { CronEnum } from '../../../common/enums/cron.enum';
 
 @Controller('crime-classification')
 export class CrimeClassificationController {
   constructor(private readonly classificationService: ClassifierService) {}
 
+  @Cron(CronEnum.EVERY_6_HOURS)
   @Post('analyze')
   analyzeText() {
     this.classificationService.execute();
